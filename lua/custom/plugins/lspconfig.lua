25655e22b6b218be838522dd777e33f530ba0528
wochap <gean.marroquin@gmail.com>
2021-12-13 19:46:08 -0500

    feat: add more servers

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -3,7 +3,7 @@ local M = {}
 M.setup_lsp = function(attach, capabilities)
   local lspconfig = require "lspconfig"
   -- TODO: add clangd vimls pyls_ms jsonls?
-  local servers = { "html", "cssls" }
+  local servers = { "html", "cssls", "emmet_ls", "jsonls", "svelte", "tailwindcss" }
 
   -- html, css
   for _, lsp in ipairs(servers) do
@@ -18,13 +18,12 @@ M.setup_lsp = function(attach, capabilities)
    
   -- typescript
   lspconfig.tsserver.setup {
+    root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json"),
     on_attach = function(client, bufnr)
       -- disable tsserver's inbuilt formatting 
-      -- since I use null-ls with denofmt for formatting ts/js stuff.
+      -- since I use null-ls for formatting
       client.resolved_capabilities.document_formatting = false
-
-      -- add keybinding for lsp formatting
-      vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", {})
+      client.resolved_capabilities.document_range_formatting = false
     end,
   }
 end
