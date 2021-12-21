6581a28b2e41eb5e80ef3c1d58f3749743e7ecc5
wochap <gean.marroquin@gmail.com>
2021-12-21 10:38:15 -0500

    custom config for nvimtree

--- lua/custom/plugins/configs/nvimtree.lua
+++ lua/custom/plugins/configs/nvimtree.lua
@@ -7,9 +7,10 @@ end
 
 local g = vim.g
 
+g.nvim_tree_group_empty = 1
 g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
 g.nvim_tree_git_hl = git_status
-g.nvim_tree_highlight_opened_files = 0
+g.nvim_tree_highlight_opened_files = 1
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
 g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
@@ -66,6 +67,8 @@ nvimtree.setup {
       allow_resize = true,
       side = "left",
       width = 25,
+      number = true,
+      relativenumber = true,
    },
 
    git = {
