0b06256a41e1344c424edd4050c681e11f7d575e
wochap <gean.marroquin@gmail.com>
2021-12-24 23:42:35 -0500

    todos

--- lua/custom/plugins/configs/todo-comments.lua
+++ lua/custom/plugins/configs/todo-comments.lua
@@ -9,6 +9,11 @@ local M = {}
 M.setup = function(on_attach)
   todo_comments.setup {
     signs = false,
+    highlight = {
+      before = "fg", -- "fg" or "bg" or empty
+      keyword = "fg", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
+      after = "fg", -- "fg" or "bg" or empty
+    },
   }
 end
 
