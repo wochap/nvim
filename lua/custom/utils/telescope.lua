d1c333528be22e17e762419c2dc4c528c7df7660
wochap <gean.marroquin@gmail.com>
2022-01-05 08:42:59 -0500

    style

--- lua/custom/utils/telescope.lua
+++ lua/custom/utils/telescope.lua
@@ -1,23 +1,21 @@
-
-  
-local telescope = require('telescope')
-local actions = require('telescope.actions')
+local telescope = require "telescope"
+local actions = require "telescope.actions"
 local M = {}
 
 M.branches = function()
-  require'telescope.builtin'.git_branches({ 
-    attach_mappings = function(_, map)
-      map('i', '<C-j>', actions.git_create_branch)
-      map('n', '<C-j>', actions.git_create_branch)
-      return true
-    end
-  })
+   require("telescope.builtin").git_branches {
+      attach_mappings = function(_, map)
+         map("i", "<C-j>", actions.git_create_branch)
+         map("n", "<C-j>", actions.git_create_branch)
+         return true
+      end,
+   }
 end
 
 M.symbols = function()
-  require("telescope.builtin").lsp_document_symbols({
-    symbols = { "Class", "Function", "Method", "Constructor", "Interface", "Module" },
-  })
+   require("telescope.builtin").lsp_document_symbols {
+      symbols = { "Class", "Function", "Method", "Constructor", "Interface", "Module" },
+   }
 end
 
 return M
