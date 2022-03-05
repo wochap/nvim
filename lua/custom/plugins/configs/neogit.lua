62462c966d556df58fc41500d0f8d514c98d9cb8
wochap <gean.marroquin@gmail.com>
2022-03-04 21:51:52 -0500

    feat: disable neogit confirm

--- lua/custom/plugins/configs/neogit.lua
+++ lua/custom/plugins/configs/neogit.lua
@@ -6,52 +6,53 @@ end
 local M = {}
 
 M.setup = function(on_attach)
-  neogit.setup {
-    kind = "split",
-    integrations = {
-      diffview = true,
-    },
-    signs = {
-      section = { "", "" },
-      item = { "", "" },
-      hunk = { "", "" },
-    },
-    sections = {
-      untracked = {
-        folded = false
+   neogit.setup {
+      disable_commit_confirmation = true,
+      kind = "split",
+      integrations = {
+         diffview = true,
       },
-      unstaged = {
-        folded = false
+      signs = {
+         section = { "", "" },
+         item = { "", "" },
+         hunk = { "", "" },
       },
-      staged = {
-        folded = false
+      sections = {
+         untracked = {
+            folded = false,
+         },
+         unstaged = {
+            folded = false,
+         },
+         staged = {
+            folded = false,
+         },
+         stashes = {
+            folded = false,
+         },
+         unpulled = {
+            folded = false,
+         },
+         unmerged = {
+            folded = false,
+         },
+         recent = {
+            folded = false,
+         },
       },
-      stashes = {
-        folded = false
+      mappings = {
+         status = {
+            ["1"] = "",
+            ["2"] = "",
+            ["3"] = "",
+            ["4"] = "",
+            -- ["1"] = "Depth1",
+            -- ["2"] = "Depth2",
+            -- ["3"] = "Depth3",
+            -- ["4"] = "Depth4",
+         },
       },
-      unpulled = {
-        folded = false
-      },
-      unmerged = {
-        folded = false
-      },
-      recent = {
-        folded = false
-      },
-    },
-    mappings = {
-      status = {
-        ["1"] = "",
-        ["2"] = "",
-        ["3"] = "",
-        ["4"] = "",
-        -- ["1"] = "Depth1",
-        -- ["2"] = "Depth2",
-        -- ["3"] = "Depth3",
-        -- ["4"] = "Depth4",
-      },
-    },
-  }
+   }
 end
 
 return M
