93f615ad95a19d3ea0aa2756dc8268c281f78205
wochap <gean.marroquin@gmail.com>
2023-04-15 16:35:39 -0500

    ui

--- lua/custom/plugins/overrides/statusline.lua
+++ lua/custom/plugins/overrides/statusline.lua
@@ -20,32 +20,9 @@ return {
     local sep_style = vim.g.statusline_sep_style
     local separators = (type(sep_style) == "table" and sep_style)
       or require("nvchad_ui.icons").statusline_separators[sep_style]
-    local sep_l = separators["left"]
     local sep_r = separators["right"]
-    local st_modules = require "nvchad_ui.statusline.modules"
 
     return {
-      git = function()
-        if not vim.b.gitsigns_head or vim.b.gitsigns_git_status then
-          return ""
-        end
-
-        local git_status = vim.b.gitsigns_status_dict
-
-        local added = (git_status.added and git_status.added ~= 0) and ("  " .. git_status.added) or ""
-        local changed = (git_status.changed and git_status.changed ~= 0) and ("  " .. git_status.changed) or ""
-        local removed = (git_status.removed and git_status.removed ~= 0) and ("  " .. git_status.removed) or ""
-        local branch_name = "   " .. git_status.head .. " "
-
-        return "%#St_gitIcons#"
-          .. branch_name
-          .. "%#St_gitAdded#"
-          .. added
-          .. "%#St_gitChanged#"
-          .. changed
-          .. "%#St_gitRemoved#"
-          .. removed
-      end,
       fileInfo = function()
         local icon = "  "
         local filename = (fn.expand "%" == "" and "Empty ") or fn.expand "%:t"
