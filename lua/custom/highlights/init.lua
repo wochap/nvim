572518ef1616ff26987931f38057fe2b205d1728
wochap <gean.marroquin@gmail.com>
2023-09-07 14:37:26 -0500

    rice: statusline

--- lua/custom/highlights/init.lua
+++ lua/custom/highlights/init.lua
@@ -1,28 +1,9 @@
 local theme = require "custom.highlights.catppuccin-mocha"
 local integrations = require "custom.highlights.integrations"
 local syntax = require "custom.highlights.syntax"
-local syntaxSpecific = require "custom.highlights.syntax-specific"
+local syntaxCatppuccin = require "custom.highlights.syntax-catppuccin"
 
-local hls = vim.tbl_deep_extend("force", integrations, syntax, syntaxSpecific)
-
--- if vim.fn.has "nvim" then
---    vim.g.terminal_color_0 = "#" .. "21222C"
---    vim.g.terminal_color_1 = "#" .. "FF5555"
---    vim.g.terminal_color_2 = "#" .. "50FA7B"
---    vim.g.terminal_color_3 = "#" .. "F1FA8C"
---    vim.g.terminal_color_4 = "#" .. "BD93F9"
---    vim.g.terminal_color_5 = "#" .. "FF79C6"
---    vim.g.terminal_color_6 = "#" .. "8BE9FD"
---    vim.g.terminal_color_7 = "#" .. "F8F8F2"
---    vim.g.terminal_color_8 = "#" .. "6272A4"
---    vim.g.terminal_color_9 = "#" .. "FF6E6E"
---    vim.g.terminal_color_10 = "#" .. "69FF94"
---    vim.g.terminal_color_11 = "#" .. "FFFFA5"
---    vim.g.terminal_color_12 = "#" .. "D6ACFF"
---    vim.g.terminal_color_13 = "#" .. "FF92DF"
---    vim.g.terminal_color_14 = "#" .. "A4FFFF"
---    vim.g.terminal_color_15 = "#" .. "FFFFFF"
--- end
+local hls = vim.tbl_deep_extend("force", integrations, syntaxCatppuccin, syntax)
 
 return {
   theme_toggle = {},
--- lua/custom/highlights/syntax.lua
+++ lua/custom/highlights/syntax.lua
@@ -2,7 +2,6 @@ local utils = require "custom.highlights.utils.init"
 local theme = require "custom.highlights.catppuccin-mocha"
 local C = theme.palette
 
--- overrides so nvim theme matches https://github.com/catppuccin/vscode
 local M = {
   -- source: https://github.com/nvim-treesitter/nvim-treesitter/commit/42ab95d5e11f247c6f0c8f5181b02e816caa4a4f#commitcomment-87014462
   ["@constant.javascript"] = { link = "Variable" },
@@ -14,57 +13,52 @@ local M = {
   ["@type.python"] = { link = "Variable" },
   ["@variable"] = { link = "Variable" },
 
-  -- source: https://github.com/catppuccin/nvim/blob/main/lua/catppuccin/groups/editor.lua
-  Conceal = { fg = C.overlay1 }, -- placeholder characters substituted for concealed text (see 'conceallevel')
-  Cursor = { fg = C.base, bg = C.text }, -- character under the cursor
-  lCursor = { fg = C.base, bg = C.text }, -- the character under the cursor when |language-mapping| is used (see 'guicursor')
-  CursorIM = { fg = C.base, bg = C.text }, -- like Cursor, but used when in IME mode |CursorIM|
-  CursorColumn = { bg = C.mantle }, -- Screen-column at the cursor, when 'cursorcolumn' is set.
-  CursorLine = {
-    bg = utils.darken(C.surface0, 0.64, C.base),
-  }, -- Screen-line at the cursor, when 'cursorline' is set.  Low-priority if forecrust (ctermfg OR guifg) is not set.
-  VertSplit = { fg = C.crust }, -- the column separating vertically split windows
-  Substitute = { bg = C.surface1, fg = C.pink }, -- |:substitute| replacement text highlighting
-  MatchParen = { fg = C.peach, bg = C.surface1 }, -- The character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
-  Search = { bg = utils.darken(C.sky, 0.30, C.base), fg = C.text }, -- Last search pattern highlighting (see 'hlsearch').  Also used for similar items that need to stand out.
-  IncSearch = { bg = utils.darken(C.sky, 0.90, C.base), fg = C.mantle }, -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
-  CurSearch = { bg = C.red, fg = C.mantle }, -- 'cursearch' highlighting: highlights the current search you're on differently
-  SpecialKey = { link = "NonText" }, -- Unprintable characters: text displayed differently from what it really is.  But not 'listchars' textspace. |hl-Whitespace|
-  NonText = { fg = C.overlay0 }, -- '@' at the end of the window, characters from 'showbreak' and other characters that do not really exist in the text (e.g., ">" displayed when a double-wide character doesn't fit at the end of the line). See also |hl-EndOfBuffer|.
+  PreProc = { link = "Comment" },
+  Tag = { fg = C.blue },
+  TagAttribute = { fg = C.yellow },
+  Todo = { fg = C.blue, bg = C.base },
+  zshCommands = { fg = C.blue },
+  zshDeref = { fg = C.red },
+  zshFunction = { fg = C.blue },
+  zshShortDeref = { fg = C.text },
+  zshSubst = { fg = C.red },
+  zshSubstDelim = { fg = C.text },
+  zshSubstQuoted = { fg = C.red },
+  zshVariableDef = { fg = C.red },
 
-  Comment = { fg = C.overlay0 }, -- just comments
-  SpecialComment = { link = "Special" }, -- special things inside a comment
-  Constant = { fg = C.peach }, -- (preferred) any constant
-  String = { fg = C.green }, -- a string constant: "this is a string"
-  Character = { fg = C.teal }, --  a character constant: 'c', '\n'
-  Number = { fg = C.peach }, --   a number constant: 234, 0xff
-  Float = { link = "Number" }, --    a floating point constant: 2.3e10
-  Boolean = { fg = C.peach }, --  a boolean constant: TRUE, false
-  Identifier = { fg = C.flamingo }, -- (preferred) any variable name
-  Function = { fg = C.blue }, -- function name (also: methods for classes)
-  Statement = { fg = C.mauve }, -- (preferred) any statement
-  Conditional = { fg = C.mauve }, --  if, then, else, endif, switch, etc.
-  Repeat = { fg = C.mauve }, --   for, do, while, etc.
-  Label = { fg = C.sapphire }, --    case, default, etc.
-  Operator = { fg = C.sky }, -- "sizeof", "+", "*", etc.
-  Keyword = { fg = C.mauve }, --  any other keyword
-  Exception = { fg = C.mauve }, --  try, catch, throw
-
-  PreProc = { fg = C.pink }, -- (preferred) generic Preprocessor
-  Include = { fg = C.mauve }, --  preprocessor #include
-  Define = { link = "PreProc" }, -- preprocessor #define
-  Macro = { fg = C.mauve }, -- same as Define
-  PreCondit = { link = "PreProc" }, -- preprocessor #if, #else, #endif, etc.
-
-  StorageClass = { fg = C.yellow }, -- static, register, volatile, etc.
-  Structure = { fg = C.yellow }, --  struct, union, enum, etc.
-  Special = { fg = C.pink }, -- (preferred) any special symbol
-  Type = { fg = C.yellow }, -- (preferred) int, long, char, etc.
-  Typedef = { link = "Type" }, --  A typedef
-  SpecialChar = { link = "Special" }, -- special character in a constant
-  Tag = { fg = C.lavender }, -- you can use CTRL-] on this
-  Delimiter = { fg = C.overlay2 }, -- character that needs attention
-  Debug = { link = "Special" }, -- debugging statements
+  ["@boolean.javascript"] = { fg = C.peach },
+  ["@boolean.nix"] = { fg = C.red },
+  ["@boolean.python"] = { fg = C.mauve },
+  ["@constant.builtin.javascript"] = { fg = C.mauve },
+  ["@constant.builtin.nix"] = { fg = C.red },
+  ["@constant.builtin.python"] = { fg = C.mauve },
+  ["@constructor.javascript"] = { fg = C.blue },
+  ["@field.nix"] = { fg = C.yellow },
+  ["@function.builtin.python"] = { fg = C.peach },
+  ["@function.call.javascript"] = { fg = C.blue },
+  ["@function.call.nix"] = { fg = C.maroon },
+  ["@keyword.operator.python"] = { fg = C.mauve },
+  ["@parameter.javascript"] = { fg = C.maroon },
+  ["@parameter.nix"] = { fg = C.maroon },
+  ["@punctuation.bracket.nix"] = { fg = C.white },
+  ["@punctuation.special.markdown"] = { fg = C.teal },
+  ["@string.special.nix"] = { fg = C.green },
+  ["@tag.delimiter"] = { fg = C.teal },
+  ["@text.literal.markdown_inline"] = { fg = C.green },
+  ["@text.reference.markdown_inline"] = { fg = C.lavender },
+  ["@text.strong.markdown_inline"] = { fg = C.red },
+  ["@text.title.1.markdown"] = { fg = C.red },
+  ["@text.title.1.marker.markdown"] = { fg = C.red },
+  ["@text.title.2.markdown"] = { fg = C.peach },
+  ["@text.title.2.marker.markdown"] = { fg = C.peach },
+  ["@text.title.3.markdown"] = { fg = C.yellow },
+  ["@text.title.3.marker.markdown"] = { fg = C.yellow },
+  ["@text.title.4.markdown"] = { fg = C.green },
+  ["@text.title.4.marker.markdown"] = { fg = C.green },
+  ["@text.title.5.markdown"] = { fg = C.blue },
+  ["@text.title.5.marker.markdown"] = { fg = C.blue },
+  ["@text.uri.markdown_inline"] = { fg = C.blue },
+  ["@type.builtin.python"] = { fg = C.peach },
 }
 
 return M
