11523c41c7c22f9be4251365d921fb5cc5652cb7
wochap <gean.marroquin@gmail.com>
2023-09-07 21:58:17 -0500

    refactor

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -257,10 +257,16 @@ M.blankline = {
 M.utils = {
   n = {
     ["<C-e>"] = { "<cmd>lua require'custom.utils.window'.closeAllFloating()<CR>", "close floating windows" },
+    ["<f2>"] = { "<cmd>lua require('base46').load_all_highlights()<CR>", "load custom theme without cache?" },
+    ["<f3>"] = { "<cmd>lua require'custom.utils.others'.printSyntaxInfo()<CR>", "print syntax info" },
+    ["<f4>"] = { "<cmd>lua require'custom.utils.others'.printBufInfo()<CR>", "print buffer info" },
   },
   i = {
     ["<C-e>"] = { "<cmd>lua require'custom.utils.window'.closeAllFloating()<CR>", "close floating windows" },
     ["<C-k>"] = { "<cmd>lua require'custom.utils.others'.expandSnippet()<CR>", "expand snippet" },
+  },
+  v = {
+    ["R"] = { "\"zy:'<,'>s/<C-r>z/" },
   },
 }
 
@@ -328,9 +334,6 @@ M.git = {
 M.persistence = {
   n = {
     ["<leader>nl"] = { "<cmd>lua require('persistence').load()<CR>", "load last session" },
-    ["<f2>"] = { "<cmd>lua require('base46').load_all_highlights()<CR>", "load custom theme without cache?" },
-    ["<f3>"] = { "<cmd>lua require'custom.utils.others'.printSyntaxInfo()<CR>", "print syntax info" },
-    ["<f4>"] = { "<cmd>lua require'custom.utils.others'.printBufInfo()<CR>", "print buffer info" },
   },
 }
 
