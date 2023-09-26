4e1a27e708b3cfcb4d76fdd58d3137da2fa0252a
wochap <gean.marroquin@gmail.com>
2023-09-25 23:50:27 -0500

    rice

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -175,6 +175,22 @@ local plugins = {
     end,
   },
   {
+    "stevearc/dressing.nvim",
+    lazy = true,
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require("lazy").load { plugins = { "dressing.nvim" } }
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require("lazy").load { plugins = { "dressing.nvim" } }
+        return vim.ui.input(...)
+      end
+    end,
+  },
+  {
     "sindrets/diffview.nvim",
     cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles", "DiffviewFileHistory" },
     config = function()
