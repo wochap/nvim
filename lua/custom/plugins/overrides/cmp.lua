d10534d6a8c8d4e727fc8c7504c7019cd90fed35
wochap <gean.marroquin@gmail.com>
2023-09-28 17:41:07 -0500

    fix cmp

--- lua/custom/plugins/overrides/cmp.lua
+++ lua/custom/plugins/overrides/cmp.lua
@@ -1,24 +1,87 @@
 local cmp = require "cmp"
+local cmp_ui = require("core.utils").load_config().ui.cmp
+local cmp_style = cmp_ui.style
 
-local options = {
+local M = {}
+
+M.options = {
   formatting = {
-    format = function(_, vim_item)
+    format = function(_, item)
       local icons = require "nvchad.icons.lspkind"
-      vim_item.kind = string.format("%s %s", icons[vim_item.kind], vim_item.kind)
+      local icon = (cmp_ui.icons and icons[item.kind]) or ""
 
-      -- limit str length
-      if string.len(vim_item.abbr) > 60 then
-        vim_item.abbr = string.format("%s...", string.sub(vim_item.abbr, 1, 60))
+      if cmp_style == "atom" or cmp_style == "atom_colored" then
+        icon = " " .. icon .. " "
+        item.menu = cmp_ui.lspkind_text and "   (" .. item.kind .. ")" or ""
+        item.kind = icon
+      else
+        icon = cmp_ui.lspkind_text and (" " .. icon .. " ") or icon
+        item.kind = string.format("%s %s", icon, cmp_ui.lspkind_text and item.kind or "")
       end
 
-      return vim_item
+      -- limit str length
+      if string.len(item.abbr) > 60 then
+        item.abbr = string.format("%s...", string.sub(item.abbr, 1, 60))
+      end
+
+      return item
     end,
   },
   mapping = {
     ["<C-f>"] = nil,
     ["<C-u>"] = cmp.mapping.scroll_docs(-4),
     ["<C-d>"] = cmp.mapping.scroll_docs(4),
+    ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+    ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
+      elseif require("luasnip").jumpable(1) then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-next", true, true, true), "")
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item { behavior = cmp.SelectBehavior.Select }
+      elseif require("luasnip").jumpable(-1) then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
+  },
+  experimental = {
+    ghost_text = false,
+    native_menu = false,
   },
 }
 
-return options
+M.setup = function(opts)
+  cmp.setup.cmdline({ "/", "?" }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = "buffer", max_item_count = 10 },
+      { name = "cmdline_history", max_item_count = 10 },
+    },
+  })
+
+  cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = "cmdline", max_item_count = 10 },
+      { name = "cmdline_history", max_item_count = 10 },
+      { name = "path", max_item_count = 10 },
+      { name = "nvim_lua", max_item_count = 10 },
+    },
+  })
+end
+
+return M
