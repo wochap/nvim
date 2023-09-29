3f46deacdf3c47d0ff3019f13aec595018face21
wochap <gean.marroquin@gmail.com>
2023-09-29 13:38:33 -0500

    f

--- lua/custom/utils/debugHelper.lua
+++ lua/custom/utils/debugHelper.lua
@@ -3,6 +3,17 @@ local dap = require "dap"
 local function attach()
   vim.notify "Attaching"
 
+  -- Requires NodeJS 18
+  -- dap.run {
+  --   type = "pwa-node",
+  --   request = "attach",
+  --   cwd = "${workspaceFolder}",
+  --   sourceMaps = true,
+  --   protocol = "inspector",
+  --   skipFiles = { "<node_internals>/**/*.js" },
+  -- }
+
+  -- Works on NodeJS 14
   dap.run {
     type = "node2",
     request = "attach",
