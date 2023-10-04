796b276ff7dc2d27348fcc396940caa8b811a668
wochap <gean.marroquin@gmail.com>
2023-10-04 08:11:00 -0500

    go

--- lua/custom/extras-lang/go.lua
+++ lua/custom/extras-lang/go.lua
@@ -23,7 +23,7 @@ local plugins = {
       end
 
       if type(opts.handlers) == "table" then
-        opts.handlers["gopls"] = function()
+        opts.handlers.gopls = function()
           local serverOpts = get_opts()
 
           serverOpts.on_attach = function(client, bufnr)
@@ -116,7 +116,28 @@ local plugins = {
     "mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "gomodifytags", "impl", "delve" })
+      vim.list_extend(opts.ensure_installed, { "gomodifytags", "impl" })
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    optional = true,
+    dependencies = {
+      {
+        "leoluz/nvim-dap-go",
+        config = true,
+      },
+    },
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, {
+          "delve",
+        })
+      end
+
+      if type(opts.handlers) == "table" then
+        opts.handlers.delve = function() end
+      end
     end,
   },
 }
