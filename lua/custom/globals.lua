f8c4680efeb3a4b968ec592ca10646b79a82a5b0
wochap <gean.marroquin@gmail.com>
2023-10-08 08:41:53 -0500

    refactor: use lua api

--- lua/custom/globals.lua
+++ lua/custom/globals.lua
@@ -18,35 +18,33 @@ opt.pumheight = 15
 opt.cursorcolumn = true
 -- set cursor style to underline
 -- opt.guicursor = "n-v-c-sm:hor20-Cursor,i-ci-ve:ver25,r-cr-o:hor20"
+opt.guifont = "Iosevka NF:h10:#e-antialias"
 
 vim.cmd [[
-  set guifont=FiraCode\ Nerd\ Font\ Ret:h10:#e-antialias
-  set linespace=13
-
-  let g:matchup_matchparen_offscreen = {'method': 'popup'}
-
-  let g:terminal_color_0 = '#45475A'
-  let g:terminal_color_1 = '#F38BA8'
-  let g:terminal_color_2 = '#A6E3A1'
-  let g:terminal_color_3 = '#F9E2AF'
-  let g:terminal_color_4 = '#89B4FA'
-  let g:terminal_color_5 = '#F5C2E7'
-  let g:terminal_color_6 = '#94E2D5'
-  let g:terminal_color_7 = '#BAC2DE'
-
-  let g:terminal_color_8 = '#585B70'
-  let g:terminal_color_9 = '#F38BA8'
-  let g:terminal_color_10 = '#A6E3A1'
-  let g:terminal_color_11 = '#F9E2AF'
-  let g:terminal_color_12 = '#89B4FA'
-  let g:terminal_color_13 = '#F5C2E7'
-  let g:terminal_color_14 = '#94E2D5'
-  let g:terminal_color_15 = '#A6ADC8'
+  " set linespace=13
 ]]
+
+g.matchup_matchparen_offscreen = { method = "popup" }
+
+g.terminal_color_0 = "#45475A"
+g.terminal_color_1 = "#F38BA8"
+g.terminal_color_2 = "#A6E3A1"
+g.terminal_color_3 = "#F9E2AF"
+g.terminal_color_4 = "#89B4FA"
+g.terminal_color_5 = "#F5C2E7"
+g.terminal_color_6 = "#94E2D5"
+g.terminal_color_7 = "#BAC2DE"
+
+g.terminal_color_8 = "#585B70"
+g.terminal_color_9 = "#F38BA8"
+g.terminal_color_10 = "#A6E3A1"
+g.terminal_color_11 = "#F9E2AF"
+g.terminal_color_12 = "#89B4FA"
+g.terminal_color_13 = "#F5C2E7"
+g.terminal_color_14 = "#94E2D5"
+g.terminal_color_15 = "#A6ADC8"
 
 -- Use nvim as git editor in lazygit
-vim.cmd [[
-  if has('nvim') && executable('nvr')
-    let $GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
-  endif
-]]
+if vim.fn.has "nvim" == 1 and vim.fn.executable "nvr" == 1 then
+  vim.env.GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
+end
