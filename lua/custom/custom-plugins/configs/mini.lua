63f3983be415fea290d836c3d5d81f27e75dd8c3
wochap <gean.marroquin@gmail.com>
2023-10-30 15:54:03 -0500

    update mappings, prefix with g

--- lua/custom/custom-plugins/configs/mini.lua
+++ lua/custom/custom-plugins/configs/mini.lua
@@ -3,13 +3,13 @@ local M = {}
 M.setup = function()
   require("mini.surround").setup {
     mappings = {
-      add = "sa", -- Add surrounding in Normal and Visual modes
-      delete = "sd", -- Delete surrounding
-      find = "sf", -- Find surrounding (to the right)
-      find_left = "sF", -- Find surrounding (to the left)
-      highlight = "sh", -- Highlight surrounding
-      replace = "sr", -- Replace surrounding
-      update_n_lines = "sn", -- Update `n_lines`
+      add = "gsa", -- Add surrounding in Normal and Visual modes
+      delete = "gsd", -- Delete surrounding
+      find = "gsf", -- Find surrounding (to the right)
+      find_left = "gsF", -- Find surrounding (to the left)
+      highlight = "gsh", -- Highlight surrounding
+      replace = "gsr", -- Replace surrounding
+      update_n_lines = "gsn", -- Update `n_lines`
 
       suffix_last = "l", -- Suffix to search with "prev" method
       suffix_next = "n", -- Suffix to search with "next" method
