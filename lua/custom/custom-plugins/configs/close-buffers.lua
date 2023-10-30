c04cf95b0585502d498b33f588c1baf9b8f3e275
wochap <gean.marroquin@gmail.com>
2023-10-30 15:53:31 -0500

    fix: close_buffers doesnt change to previous buffer

--- lua/custom/custom-plugins/configs/close-buffers.lua
+++ lua/custom/custom-plugins/configs/close-buffers.lua
@@ -1,4 +1,3 @@
-local tabufline = require "nvchad.tabufline"
 local M = {}
 local function string_in_list(s, list)
   for _, v in ipairs(list) do
@@ -11,8 +10,10 @@ end
 
 M.options = {
   next_buffer_cmd = function(windows)
+    local tabufline = require "nvchad.tabufline"
     local bufs = tabufline.bufilter() or {}
-    local alt_bufnr = vim.fn.getreg "#"
+    local alt_bufname = vim.fn.getreg "#"
+    local alt_bufnr = vim.fn.bufnr(alt_bufname)
     tabufline.tabuflineNext()
     if string_in_list(alt_bufnr, bufs) then
       vim.cmd("b " .. alt_bufnr)
