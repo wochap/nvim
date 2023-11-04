78c92a9242b98da6a5e329c22342ad897497e929
wochap <gean.marroquin@gmail.com>
2023-11-04 12:28:53 -0500

    rice

--- lua/custom/custom-plugins/configs/nvim-dap.lua
+++ lua/custom/custom-plugins/configs/nvim-dap.lua
@@ -6,11 +6,11 @@ M.setup = function(opts)
 
   vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
 
-  vim.fn.sign_define("DapBreakpoint", { text = " ", texthl = "DiagnosticInfo", linehl = "", numhl = "" })
-  vim.fn.sign_define("DapBreakpointCondition", { text = " ", texthl = "DiagnosticInfo", linehl = "", numhl = "" })
+  vim.fn.sign_define("DapBreakpoint", { text = " ", texthl = "DiagnosticError", linehl = "", numhl = "" })
+  vim.fn.sign_define("DapBreakpointCondition", { text = " ", texthl = "DiagnosticError", linehl = "", numhl = "" })
   vim.fn.sign_define("DapBreakpointRejected", { text = " ", texthl = "DiagnosticError", linehl = "", numhl = "" })
   vim.fn.sign_define("DapStopped", { text = "󰁕 ", texthl = "DiagnosticWarn", linehl = "DapStoppedLine", numhl = "" })
-  vim.fn.sign_define("DapLogPoint", { text = ".>", texthl = "DiagnosticInfo", linehl = "", numhl = "" })
+  vim.fn.sign_define("DapLogPoint", { text = ".>", texthl = "DiagnosticError", linehl = "", numhl = "" })
 end
 
 return M
