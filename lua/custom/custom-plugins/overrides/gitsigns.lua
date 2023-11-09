1ba8d5c4eed77540b564b927cc0432e8391a5009
wochap <gean.marroquin@gmail.com>
2023-11-08 22:51:03 -0500

    feat: rice gitsigns

--- lua/custom/custom-plugins/overrides/gitsigns.lua
+++ lua/custom/custom-plugins/overrides/gitsigns.lua
@@ -5,32 +5,26 @@ M.options = {
     add = {
       hl = "GitSignsAdd",
       text = "▍",
-      numhl = "GitSignsAddNr",
-      linehl = "GitSignsAddLn",
     },
     change = {
       hl = "GitSignsChange",
       text = "▍",
-      numhl = "GitSignsChangeNr",
-      linehl = "GitSignsChangeLn",
     },
     delete = {
       hl = "GitSignsDelete",
-      text = "~",
-      numhl = "GitSignsDeleteNr",
-      linehl = "GitSignsDeleteLn",
+      text = "",
     },
     changedelete = {
       hl = "GitSignsChange",
-      text = "~",
-      numhl = "GitSignsChangeNr",
-      linehl = "GitSignsChangeLn",
+      text = "▍",
     },
     topdelete = {
       hl = "GitSignsDelete",
-      text = "‾",
-      numhl = "GitSignsDeleteNr",
-      linehl = "GitSignsDeleteLn",
+      text = "",
+    },
+    untracked = {
+      hl = "GitSignsAdd",
+      text = "▍",
     },
   },
 }
