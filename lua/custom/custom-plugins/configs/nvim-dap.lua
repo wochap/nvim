0e7a9c66dfb2050a81f65a9237b7c7523b82ee6e
wochap <gean.marroquin@gmail.com>
2023-11-10 22:29:07 -0500

    fix dap .vscode/launch.json load

--- lua/custom/custom-plugins/configs/nvim-dap.lua
+++ lua/custom/custom-plugins/configs/nvim-dap.lua
@@ -2,7 +2,8 @@ local M = {}
 
 M.setup = function(opts)
   -- Load vscode launchjs
-  require("dap.ext.vscode").load_launchjs()
+  local filetypes = require "mason-nvim-dap.mappings.filetypes"
+  require("dap.ext.vscode").load_launchjs(nil, filetypes)
 
   vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
 
