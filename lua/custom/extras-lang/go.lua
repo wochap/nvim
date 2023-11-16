b6981f1b9d20253a9cb9f6948f2d54cf886778bb
wochap <gean.marroquin@gmail.com>
2023-11-16 12:51:21 -0500

    refactor

--- lua/custom/extras-lang/go.lua
+++ lua/custom/extras-lang/go.lua
@@ -1,16 +1,5 @@
 local plugins = {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    optional = true,
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "go",
-        "gomod",
-        "gowork",
-        "gosum",
-      })
-    end,
-  },
+  { import = "lazyvim.plugins.extras.lang.go" },
 
   {
     "neovim/nvim-lspconfig",
@@ -18,104 +7,28 @@ local plugins = {
     opts = {
       servers = {
         gopls = {
-          settings = {
-            gopls = {
-              gofumpt = true,
-              codelenses = {
-                gc_details = false,
-                generate = true,
-                regenerate_cgo = true,
-                run_govulncheck = true,
-                test = true,
-                tidy = true,
-                upgrade_dependency = true,
-                vendor = true,
-              },
-              hints = {
-                assignVariableTypes = true,
-                compositeLiteralFields = true,
-                compositeLiteralTypes = true,
-                constantValues = true,
-                functionTypeParameters = true,
-                parameterNames = true,
-                rangeVariableTypes = true,
-              },
-              analyses = {
-                fieldalignment = true,
-                nilness = true,
-                unusedparams = true,
-                unusedwrite = true,
-                useany = true,
-              },
-              usePlaceholders = true,
-              completeUnimported = true,
-              staticcheck = true,
-              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
-              semanticTokens = true,
-            },
+          keys = {
+            { "<leader>td", false },
+            -- Workaround for the lack of a DAP strategy in neotest-go: https://github.com/nvim-neotest/neotest-go/issues/12
+            { "<leader>dt", "<cmd>lua require('dap-go').debug_test()<CR>", desc = "Debug Nearest (Go)" },
           },
         },
       },
-      setup = {
-        gopls = function(_, opts)
-          -- workaround for gopls not supporting semanticTokensProvider
-          -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
-          require("lazyvim.util").lsp.on_attach(function(client, _)
-            if client.name == "gopls" then
-              if not client.server_capabilities.semanticTokensProvider then
-                local semantic = client.config.capabilities.textDocument.semanticTokens
-                client.server_capabilities.semanticTokensProvider = {
-                  full = true,
-                  legend = {
-                    tokenTypes = semantic.tokenTypes,
-                    tokenModifiers = semantic.tokenModifiers,
-                  },
-                  range = true,
-                }
-              end
-            end
-          end)
-          -- end workaround
-        end,
-      },
     },
   },
 
+  -- undo none-ls changes added by LazyVim
   {
-    "stevearc/conform.nvim",
-    optional = true,
-    dependencies = {
-      {
-        "williamboman/mason.nvim",
-        optional = true,
-        opts = function(_, opts)
-          vim.list_extend(opts.ensure_installed, { "goimports", "gofumpt" })
-        end,
-      },
-    },
-    opts = {
-      formatters_by_ft = {
-        go = { "goimports", "gofumpt" },
-      },
-    },
+    "nvimtools/none-ls.nvim",
+    enabled = false,
   },
-
   {
-    "mfussenegger/nvim-dap",
-    optional = true,
-    dependencies = {
-      {
-        "jay-babu/mason-nvim-dap.nvim",
-        optional = true,
-        opts = function(_, opts)
-          vim.list_extend(opts.ensure_installed, { "delve" })
-        end,
-      },
-      {
-        "leoluz/nvim-dap-go",
-        config = true,
-      },
-    },
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      local remove_str_from_list = require("custom.utils").remove_str_from_list
+      remove_str_from_list(opts.ensure_installed, "gomodifytags")
+      remove_str_from_list(opts.ensure_installed, "impl")
+    end,
   },
 }
 
