21a658988b9c5f115927cc15fbc1f34286b7c7af
wochap <gean.marroquin@gmail.com>
2023-12-05 20:50:41 -0500

    add cmake and make support

--- lua/custom/extras-lang/c.lua
+++ lua/custom/extras-lang/c.lua
@@ -1,11 +1,19 @@
 local plugins = {
   { import = "lazyvim.plugins.extras.lang.clangd" },
+  { import = "lazyvim.plugins.extras.lang.cmake" },
 
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "make" })
+    end,
+  },
   {
     "neovim/nvim-lspconfig",
     optional = true,
     opts = {
       servers = {
+        -- autotools_ls = {},
         clangd = {
           keys = {
             { "<leader>cR", false },
