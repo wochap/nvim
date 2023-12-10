bb9224806d39cd8c96d4174fd213a073933b690b
wochap <gean.marroquin@gmail.com>
2023-12-09 22:17:48 -0500

    rice zen mode

--- lua/custom/custom-plugins/configs/zen-mode.lua
+++ lua/custom/custom-plugins/configs/zen-mode.lua
@@ -3,20 +3,30 @@ local M = {}
 M.options = {
   window = {
     backdrop = 1,
-    width = 120, -- width of the Zen window
+    width = 130, -- width of the Zen window
     height = 1, -- height of the Zen window
+    options = {
+      cursorline = false,
+      cursorcolumn = false,
+    },
   },
   plugins = {
     options = {
       enabled = true,
       ruler = false, -- disables the ruler text in the cmd line area
       showcmd = false, -- disables the command in the last line of the screen
+      laststatus = 0,
     },
     twilight = { enabled = false }, -- enable to start Twilight when zen mode opens
     gitsigns = { enabled = true }, -- disables git signs
   },
-  on_open = function(win) end,
-  on_close = function() end,
+  on_open = function()
+    vim.cmd "IBLDisable"
+    vim.opt_local.foldcolumn = "0"
+  end,
+  on_close = function()
+    vim.cmd "IBLEnable"
+  end,
 }
 
 return M
