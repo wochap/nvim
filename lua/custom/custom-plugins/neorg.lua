708407e97ba9f2042257c8bf4934c8b21de94bf2
wochap <gean.marroquin@gmail.com>
2023-12-11 01:19:28 -0500

    feat: mappings for neorg mode

--- lua/custom/custom-plugins/neorg.lua
+++ lua/custom/custom-plugins/neorg.lua
@@ -5,17 +5,46 @@ local function keybinds_hook(keybinds)
 
   keybinds.map_event_to_mode("norg", {
     n = {
+      { "tu", "core.qol.todo_items.todo.task_undone", "mark undone", desc = "mark undone" },
+      { "tp", "core.qol.todo_items.todo.task_pending" },
+      { "td", "core.qol.todo_items.todo.task_done" },
+      { "th", "core.qol.todo_items.todo.task_on_hold" },
+      { "tc", "core.qol.todo_items.todo.task_cancelled" },
+      { "tr", "core.qol.todo_items.todo.task_recurring" },
+      { "ti", "core.qol.todo_items.todo.task_important" },
+      { "<A-Space>", "core.qol.todo_items.todo.task_cycle" },
       { leader .. "n", "core.dirman.new.note" },
+      { leader .. "t", ":Neorg journal today" },
+      { leader .. "T", ":Neorg journal tomorrow" },
     },
   }, {
     silent = true,
     noremap = true,
   })
+
+  local ok, wk = pcall(require, "which-key")
+  if not ok then
+    return
+  end
+  wk.register {
+    ["tu"] = "mark undone",
+    ["tp"] = "mark pending",
+    ["td"] = "mark done",
+    ["th"] = "mark on hold",
+    ["tc"] = "mark cacelled",
+    ["tr"] = "mark recurring",
+    ["ti"] = "mark important",
+
+    [leader .. "n"] = "new note",
+    [leader .. "t"] = "journal today",
+    [leader .. "T"] = "journal tomorrow",
+  }
 end
 
 return {
   {
     "nvim-neorg/neorg",
+    commit = "baaf13a3145534144b795ad37db22bfffd2ad343",
     lazy = neorg_arg ~= vim.fn.argv()[1],
     build = ":Neorg sync-parsers",
     dependencies = { "nvim-lua/plenary.nvim" },
@@ -27,6 +56,13 @@ return {
           ["core.completion"] = {
             config = {
               engine = "nvim-cmp",
+            },
+          },
+
+          ["core.qol.todo_items"] = {
+            config = {
+              create_todo_items = true,
+              create_todo_parents = true,
             },
           },
 
@@ -43,7 +79,7 @@ return {
               workspaces = {
                 home = "~/Sync/neorg",
                 journal = "~/Sync/journal",
-                gtd = "~/Sync/gtd",
+                -- gtd = "~/Sync/gtd",
               },
               default_workspace = "home",
             },
@@ -51,8 +87,8 @@ return {
 
           ["core.journal"] = {
             config = {
-              workspace = "journal",
-              journal_folder = "",
+              workspace = "home",
+              journal_folder = "journal",
             },
           },
         },
