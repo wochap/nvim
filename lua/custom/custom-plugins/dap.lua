c2ca487c33744111550b974d9e0a5b946ebe0653
wochap <gean.marroquin@gmail.com>
2023-12-17 23:55:14 -0500

    refactor

--- lua/custom/custom-plugins/dap.lua
+++ lua/custom/custom-plugins/dap.lua
@@ -1,5 +1,5 @@
+-- Debugger
 return {
-  -- Debugger
   {
     "LiadOz/nvim-dap-repl-highlights",
     build = ":TSInstall dap_repl",
@@ -9,12 +9,25 @@ return {
     "mfussenegger/nvim-dap",
     dependencies = {
       "ofirgall/goto-breakpoints.nvim",
+
       {
         "rcarriga/nvim-dap-ui",
         config = function(_, opts)
-          require("custom.custom-plugins.configs.nvim-dap-ui").setup(opts)
+          local dap = require "dap"
+          local dapui = require "dapui"
+          dapui.setup(opts)
+          dap.listeners.after.event_initialized["dapui_config"] = function()
+            dapui.open {}
+          end
+          dap.listeners.before.event_terminated["dapui_config"] = function()
+            dapui.close {}
+          end
+          dap.listeners.before.event_exited["dapui_config"] = function()
+            dapui.close {}
+          end
         end,
       },
+
       {
         "theHamsta/nvim-dap-virtual-text",
         opts = {},
@@ -34,7 +47,27 @@ return {
     },
     config = function(_, opts)
       dofile(vim.g.base46_cache .. "dap")
-      require("custom.custom-plugins.configs.nvim-dap").setup(opts)
+
+      -- Load vscode launchjs
+      local filetypes = require "mason-nvim-dap.mappings.filetypes"
+      require("dap.ext.vscode").load_launchjs(nil, filetypes)
+
+      vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
+
+      vim.fn.sign_define("DapBreakpoint", { text = " ", texthl = "DiagnosticError", linehl = "", numhl = "" })
+      vim.fn.sign_define(
+        "DapBreakpointCondition",
+        { text = " ", texthl = "DiagnosticError", linehl = "", numhl = "" }
+      )
+      vim.fn.sign_define(
+        "DapBreakpointRejected",
+        { text = " ", texthl = "DiagnosticError", linehl = "", numhl = "" }
+      )
+      vim.fn.sign_define(
+        "DapStopped",
+        { text = "󰁕 ", texthl = "DiagnosticWarn", linehl = "DapStoppedLine", numhl = "" }
+      )
+      vim.fn.sign_define("DapLogPoint", { text = ".>", texthl = "DiagnosticError", linehl = "", numhl = "" })
     end,
   },
 }
