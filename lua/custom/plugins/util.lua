1b69be654670ed7cebbd958092f515df1d398464
wochap <gean.marroquin@gmail.com>
2024-03-04 18:46:59 -0500

    add nvim-colorizer.lua

--- lua/custom/plugins/util.lua
+++ lua/custom/plugins/util.lua
@@ -183,6 +183,33 @@ return {
     },
   },
 
+  {
+    "NvChad/nvim-colorizer.lua",
+    event = "LazyFile",
+    opts = {
+      user_default_options = {
+        names = false, -- "Name" codes like Blue or blue
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        AARRGGBB = true, -- 0xAARRGGBB hex codes
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = true, -- CSS hsl() and hsla() functions
+        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        -- Available modes for `mode`: foreground, background,  virtualtext
+        mode = "virtualtext", -- Set the display mode.
+        -- Available methods are false / true / "normal" / "lsp" / "both"
+        -- True is same as normal
+        tailwind = false, -- Enable tailwind colors
+        -- parsers can contain values used in |user_default_options|
+        sass = { enable = true, parsers = { "css" } }, -- Enable sass colors
+        virtualtext = "■",
+      },
+    },
+    config = function(_, opts)
+      require("colorizer").setup(opts)
+    end,
+  },
+
   -- library used by other plugins
   { "nvim-lua/plenary.nvim" },
   { "nvim-lua/popup.nvim" },
