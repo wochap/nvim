291ff7e55dc421012779346d9bc68def4870ffa5
wochap <gean.marroquin@gmail.com>
2024-03-07 09:35:53 -0500

    remove volar on_new_config,
    As of 2.0.0, Volar no longer supports TypeScript itself

--- lua/custom/plugins/extras-lang/web.lua
+++ lua/custom/plugins/extras-lang/web.lua
@@ -155,28 +155,6 @@ return {
               },
             },
           },
-          on_new_config = function(new_config, new_root_dir)
-            local util = require "lspconfig.util"
-            local function get_typescript_server_path(root_dir)
-              local homeDir = os.getenv "HOME"
-              local global_ts = homeDir .. "/.npm/lib/node_modules/typescript/lib"
-              -- Alternative location if installed as root:
-              -- local global_ts = '/usr/local/lib/node_modules/typescript/lib'
-              local found_ts = ""
-              local function check_dir(path)
-                found_ts = util.path.join(path, "node_modules", "typescript", "lib")
-                if util.path.exists(found_ts) then
-                  return path
-                end
-              end
-              if util.search_ancestors(root_dir, check_dir) then
-                return found_ts
-              else
-                return global_ts
-              end
-            end
-            new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
-          end,
         },
         eslint = {
           settings = {
