0953c4d512f3a709ae8ea78451f00208451d0877
wochap <gean.marroquin@gmail.com>
2024-03-27 15:44:00 -0500

    docs

--- lua/custom/utils/keymaps.lua
+++ lua/custom/utils/keymaps.lua
@@ -13,6 +13,7 @@ M.close_all_floating = function()
 
   for _, win in ipairs(vim.api.nvim_list_wins()) do
     local _, config = pcall(vim.api.nvim_win_get_config, win)
+    -- TODO: close nui.nvim popups
     if config and config.relative ~= "" then
       local ok, _ = pcall(vim.api.nvim_win_close, win, false)
       vim.notify("closing window:" .. (not ok and " failed" or "") .. win)
