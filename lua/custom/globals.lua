fc1a120db94c657b13ad08aee853fa463ba46474
wochap <gean.marroquin@gmail.com>
2024-04-03 08:04:22 -0500

    rice(neovide)

--- lua/custom/globals.lua
+++ lua/custom/globals.lua
@@ -21,7 +21,7 @@ vim.g.terminal_color_15 = "#A6ADC8"
 
 if vim.g.neovide then
   vim.g.neovide_hide_mouse_when_typing = true
-  vim.g.neovide_floating_shadow = false
+  vim.g.neovide_floating_z_height = 7
   vim.g.neovide_floating_blur_amount_x = 0
   vim.g.neovide_floating_blur_amount_y = 0
 end
