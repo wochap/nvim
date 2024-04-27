c4bc6805344c5c4153a34b8181d0bf6230fda376
wochap <gean.marroquin@gmail.com>
2024-04-27 09:24:46 -0500

    fix(lang-c) add codelldb

--- lua/custom/plugins/lang-c.lua
+++ lua/custom/plugins/lang-c.lua
@@ -41,4 +41,21 @@ return {
       },
     },
   },
+
+  -- HACK: when we load lazyvim.plugins.extras.lang.clangd
+  -- mason opts.ensure_installed isn't a table yet
+  -- so codelldb isn't being added
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      -- Ensure C/C++ debugger is installed
+      "williamboman/mason.nvim",
+      optional = true,
+      opts = function(_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        vim.list_extend(opts.ensure_installed, { "codelldb" })
+      end,
+    },
+  },
 }
