6d7611ed229eb6381ee7a5433fb17e66a4bfd421
wochap <gean.marroquin@gmail.com>
2024-06-04 21:39:07 -0500

    feat(lazydev.nvim) replace neodev.nvim with lazydev.nvim

--- lua/custom/plugins/lang-lua.lua
+++ lua/custom/plugins/lang-lua.lua
@@ -11,17 +11,6 @@ return {
   {
     "neovim/nvim-lspconfig",
     optional = true,
-    dependencies = {
-      {
-        "folke/neodev.nvim",
-        opts = {
-          library = {
-            plugins = { "nvim-dap-ui" },
-            types = true,
-          },
-        },
-      },
-    },
     opts = {
       servers = {
         lua_ls = {
@@ -30,14 +19,48 @@ return {
               workspace = {
                 checkThirdParty = false,
               },
+              codeLens = {
+                enable = true,
+              },
               completion = {
                 callSnippet = "Replace",
+              },
+              doc = {
+                privateName = { "^_" },
+              },
+              hint = {
+                enable = true,
+                setType = false,
+                paramType = true,
+                paramName = "Disable",
+                semicolon = "Disable",
+                arrayIndex = "Disable",
               },
             },
           },
         },
       },
     },
+  },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    cmd = "LazyDev",
+    opts = {
+      library = {
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  {
+    "Bilal2453/luvit-meta",
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      vim.list_extend(opts.sources, { { name = "lazydev", group_index = 0 } })
+    end,
   },
 
   {
