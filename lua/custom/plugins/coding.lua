1005d7d752216cc20e4a228329e9303265a23c2d
wochap <gean.marroquin@gmail.com>
2024-07-02 09:51:25 -0500

    fix: add missing cmp-cmdline-history dependency

--- lua/custom/plugins/coding.lua
+++ lua/custom/plugins/coding.lua
@@ -230,6 +230,7 @@ return {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
+      "dmitmel/cmp-cmdline-history",
     },
     opts = function(_, opts)
       local cmp = require "cmp"
