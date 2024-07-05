3d8d0909793afcac714f1444dc7bbe9c209dd443
wochap <gean.marroquin@gmail.com>
2024-07-05 16:05:41 -0500

    feat(cmp) update max items in path source to 10

--- lua/custom/plugins/coding.lua
+++ lua/custom/plugins/coding.lua
@@ -363,7 +363,7 @@ return {
         sources = cmp.config.sources({
           { name = "luasnip", max_item_count = 10 },
           { name = "nvim_lsp" },
-          { name = "path" },
+          { name = "path", max_item_count = 10 },
         }, {
           { name = "buffer", max_item_count = 10 },
         }),
