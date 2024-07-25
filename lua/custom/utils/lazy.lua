57a2c9eb1bd325748ce032de1bbef1f15ad56379
wochap <gean.marroquin@gmail.com>
2024-07-25 10:00:59 -0500

    feat(lazy.nvim) update mappings

--- lua/custom/utils/lazy.lua
+++ lua/custom/utils/lazy.lua
@@ -32,14 +32,7 @@ end
 
 M.load_mappings = function()
   local map = keymapsUtils.map
-  map("n", "<leader>LR", "<cmd>Lazy restore<cr>", "restore")
-  map("n", "<leader>LI", "<cmd>Lazy install<cr>", "install")
-  map("n", "<leader>LS", "<cmd>Lazy sync<cr>", "sync")
-  map("n", "<leader>LC", "<cmd>Lazy check<cr>", "check")
-  map("n", "<leader>LU", "<cmd>Lazy update<cr>", "update")
-  map("n", "<leader>LP", "<cmd>Lazy profile<cr>", "profile")
-  map("n", "<leader>LH", "<cmd>Lazy<cr>", "info")
-  map("n", "<leader>Li", "<cmd>Lazy<cr>", "info")
+  map("n", "<leader>L", "<cmd>Lazy<cr>", "lazy")
 end
 
 return M
