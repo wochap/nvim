24b2b23633cca797706e9f52b2231e49dd4b4d60
wochap <gean.marroquin@gmail.com>
2024-08-17 12:27:48 -0500

    feat: add latex lang

---
+++ lua/custom/plugins/lang-texlive.lua
@@ -0,0 +1,75 @@
+local utils = require "custom.utils"
+local keymapsUtils = require "custom.utils.keymaps"
+local nvim_textlabconfig_bin_path = vim.fn.stdpath "data" .. "/lazy/nvim-texlabconfig/nvim-texlabconfig"
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = {
+      ensure_installed = { "latex" },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    optional = true,
+    init = function()
+      utils.autocmd({ "BufEnter" }, {
+        group = utils.augroup "load_texlab_mappings",
+        pattern = "*.tex",
+        callback = function()
+          local bufnr = vim.api.nvim_get_current_buf()
+          keymapsUtils.map("n", "<leader>fl", "<cmd>TexlabForward<CR>", "texlab forward", {
+            buffer = bufnr,
+          })
+        end,
+      })
+    end,
+    opts = {
+      servers = {
+        texlab = {
+          settings = {
+            texlab = {
+              -- auxDirectory = ".",
+              -- bibtexFormatter = "texlab",
+              build = {
+                -- args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
+                -- executable = "latexmk",
+                forwardSearchAfter = true,
+                onSave = true,
+              },
+              chktex = {
+                --   onEdit = false,
+                onOpenAndSave = true,
+              },
+              -- diagnosticsDelay = 300,
+              -- formatterLineLength = 80,
+              forwardSearch = {
+                executable = "zathura",
+                args = {
+                  "--synctex-editor-command",
+                  nvim_textlabconfig_bin_path .. [[ -file '%%%{input}' -line %%%{line} -server ]] .. vim.v.servername,
+                  "--synctex-forward",
+                  "%l:1:%f",
+                  "%p",
+                },
+              },
+              -- latexFormatter = "latexindent",
+              -- latexindent = {
+              --   modifyLineBreaks = false,
+              -- },
+            },
+          },
+        },
+      },
+    },
+  },
+
+  {
+    "f3fora/nvim-texlabconfig",
+    ft = { "tex", "bib" },
+    build = "go build",
+    opts = {},
+  },
+}
