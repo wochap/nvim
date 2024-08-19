30862e019ce67bf4baca566cce1610a8b4d1d9c2
wochap <gean.marroquin@gmail.com>
2024-08-19 08:52:21 -0500

    feat: add image.nvim

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -660,4 +660,34 @@ return {
       end
     end,
   },
+
+  {
+    "3rd/image.nvim",
+    enabled = constants.in_kitty,
+    event = "VeryLazy",
+    opts = {
+      backend = "kitty",
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = true,
+          download_remote_images = true,
+          only_render_image_at_cursor = true,
+          filetypes = { "markdown" },
+        },
+        neorg = {
+          enabled = false,
+        },
+      },
+      -- max_width = nil,
+      -- max_height = nil,
+      -- max_width_window_percentage = nil,
+      max_height_window_percentage = 40,
+      window_overlap_clear_enabled = true,
+      window_overlap_clear_ft_ignore = {},
+      -- editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+      -- tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+      -- hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
+    },
+  },
 }
