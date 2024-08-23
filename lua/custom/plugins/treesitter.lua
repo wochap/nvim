9e02972310b7f5ce3c69c8b0f8025ce80250d26b
wochap <gean.marroquin@gmail.com>
2024-08-23 16:59:40 -0500

    refactor(nvim-treesitter): move out dependencies

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,3 +1,4 @@
+local constants = require "custom.utils.constants"
 local utils = require "custom.utils"
 local lazyUtils = require "custom.utils.lazy"
 
@@ -5,56 +6,10 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     version = false, -- last release is way too old and doesn't work on Windows
+    lazy = not constants.has_file_arg,
     build = ":TSUpdate",
     event = { "LazyFile", "VeryLazy" },
     cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-    dependencies = {
-      {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        config = function()
-          -- When in diff mode, we want to use the default
-          -- vim text objects c & C instead of the treesitter ones.
-          local move = require "nvim-treesitter.textobjects.move"
-          local configs = require "nvim-treesitter.configs"
-          for name, fn in pairs(move) do
-            if name:find "goto" == 1 then
-              move[name] = function(q, ...)
-                if vim.wo.diff then
-                  local config = configs.get_module("textobjects.move")[name]
-                  for key, query in pairs(config or {}) do
-                    if q == query and key:find "[%]%[][cC]" then
-                      vim.cmd("normal! " .. key)
-                      return
-                    end
-                  end
-                end
-                return fn(q, ...)
-              end
-            end
-          end
-        end,
-      },
-
-      {
-        url = "https://gitlab.com/HiPhish/rainbow-delimiters.nvim.git",
-        init = function()
-          vim.g.rainbow_delimiters = {
-            query = {
-              javascript = "rainbow-parens",
-            },
-            highlight = {
-              "rainbow1",
-              "rainbow2",
-              "rainbow3",
-              "rainbow4",
-              "rainbow5",
-              "rainbow6",
-            },
-            whitelist = { "javascript" },
-          }
-        end,
-      },
-    },
     init = function(plugin)
       -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
       -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
@@ -117,6 +72,63 @@ return {
         require("nvim-treesitter.configs").setup(opts)
 
         vim.treesitter.language.register("bash", "zsh")
+      end)
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    event = "VeryLazy",
+    config = function()
+      -- If treesitter is already loaded, we need to run config again for textobjects
+      if lazyUtils.is_loaded "nvim-treesitter" then
+        local opts = lazyUtils.opts "nvim-treesitter"
+        require("nvim-treesitter.configs").setup { textobjects = opts.textobjects }
+      end
+
+      -- When in diff mode, we want to use the default
+      -- vim text objects c & C instead of the treesitter ones.
+      local move = require "nvim-treesitter.textobjects.move"
+      local configs = require "nvim-treesitter.configs"
+      for name, fn in pairs(move) do
+        if name:find "goto" == 1 then
+          move[name] = function(q, ...)
+            if vim.wo.diff then
+              local config = configs.get_module("textobjects.move")[name]
+              for key, query in pairs(config or {}) do
+                if q == query and key:find "[%]%[][cC]" then
+                  vim.cmd("normal! " .. key)
+                  return
+                end
+              end
+            end
+            return fn(q, ...)
+          end
+        end
+      end
+    end,
+  },
+
+  {
+    url = "https://gitlab.com/HiPhish/rainbow-delimiters.nvim.git",
+    event = "VeryLazy",
+    opts = {
+      query = {
+        javascript = "rainbow-parens",
+      },
+      highlight = {
+        "rainbow1",
+        "rainbow2",
+        "rainbow3",
+        "rainbow4",
+        "rainbow5",
+        "rainbow6",
+      },
+      whitelist = { "javascript" },
+    },
+    config = function(_, opts)
+      lazyUtils.on_load("nvim-treesitter", function()
+        require("rainbow-delimiters.setup").setup(opts)
       end)
     end,
   },
