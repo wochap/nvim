fffcdf8be1b40670635f891b9065ffd65470d7c5
wochap <gean.marroquin@gmail.com>
2024-08-30 09:42:31 -0500

    style:

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -1,52 +1,54 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*",
-  event = {
-    "BufReadPre " .. vim.fn.expand "~" .. "/Sync/obsidian/*.md",
-    "BufNewFile " .. vim.fn.expand "~" .. "/Sync/obsidian/*.md",
-  },
-  opts = {
-    workspaces = {
-      {
-        name = "utp",
-        path = "~/Sync/obsidian/utp",
-      },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    event = {
+      "BufReadPre " .. vim.fn.expand "~" .. "/Sync/obsidian/*.md",
+      "BufNewFile " .. vim.fn.expand "~" .. "/Sync/obsidian/*.md",
     },
-    notes_subdir = "",
-    daily_notes = {
-      folder = "journal/dailies",
-      -- template = nil,
-    },
-    mappings = {
-      ["gf"] = {
-        action = function()
-          return require("obsidian").util.gf_passthrough()
-        end,
-        opts = { noremap = false, expr = true, buffer = true },
+    opts = {
+      workspaces = {
+        {
+          name = "utp",
+          path = "~/Sync/obsidian/utp",
+        },
       },
-      ["<cr>"] = {
-        action = function()
-          return require("obsidian").util.smart_action()
-        end,
-        opts = { buffer = true, expr = true },
+      notes_subdir = "",
+      daily_notes = {
+        folder = "journal/dailies",
+        -- template = nil,
       },
-    },
-    picker = {
-      name = "telescope.nvim",
-      note_mappings = {
-        new = "<C-n>",
-        insert_link = "<C-l>",
+      mappings = {
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        ["<cr>"] = {
+          action = function()
+            return require("obsidian").util.smart_action()
+          end,
+          opts = { buffer = true, expr = true },
+        },
       },
-      tag_mappings = {
-        tag_note = "<C-n>",
-        insert_tag = "<C-l>",
+      picker = {
+        name = "telescope.nvim",
+        note_mappings = {
+          new = "<C-n>",
+          insert_link = "<C-l>",
+        },
+        tag_mappings = {
+          tag_note = "<C-n>",
+          insert_tag = "<C-l>",
+        },
       },
-    },
-    ui = {
-      enable = false,
-    },
-    attachments = {
-      img_folder = "assets/imgs",
+      ui = {
+        enable = false,
+      },
+      attachments = {
+        img_folder = "assets/imgs",
+      },
     },
   },
 }
