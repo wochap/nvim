e32c8e1871ca38bd3ddfb50cb62f3ca31ef3158a
wochap <gean.marroquin@gmail.com>
2025-01-05 08:22:32 -0500

    feat(mini.ai) add d and e text objects

--- lua/custom/plugins/coding.lua
+++ lua/custom/plugins/coding.lua
@@ -83,6 +83,7 @@ return {
     },
   },
 
+  -- Better text-objects
   {
     "echasnovski/mini.ai",
     event = { "LazyFile", "VeryLazy" },
@@ -91,21 +92,19 @@ return {
       return {
         n_lines = 500,
         custom_textobjects = {
-          o = ai.gen_spec.treesitter({
+          o = ai.gen_spec.treesitter { -- code block
             a = { "@block.outer", "@conditional.outer", "@loop.outer" },
             i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-          }, {}),
-          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
-          t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
-          g = function() -- Whole buffer, similar to `gg` and 'G' motion
-            local from = { line = 1, col = 1 }
-            local to = {
-              line = vim.fn.line "$",
-              col = math.max(vim.fn.getline("$"):len(), 1),
-            }
-            return { from = from, to = to }
-          end,
+          },
+          f = ai.gen_spec.treesitter { a = "@function.outer", i = "@function.inner" }, -- function
+          c = ai.gen_spec.treesitter { a = "@class.outer", i = "@class.inner" }, -- class
+          t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+          d = { "%f[%d]%d+" }, -- digits
+          e = { -- Word with case
+            { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
+            "^().*()$",
+          },
+          g = LazyVim.mini.ai_buffer, -- buffer
           u = ai.gen_spec.function_call(), -- u for "Usage"
           U = ai.gen_spec.function_call { name_pattern = "[%w_]" }, -- without dot in function name
         },
@@ -113,10 +112,9 @@ return {
     end,
     config = function(_, opts)
       require("mini.ai").setup(opts)
-
-      lazyUtils.on_load("which-key.nvim", function()
+      LazyVim.on_load("which-key.nvim", function()
         vim.schedule(function()
-          require("lazyvim.util.mini").ai_whichkey(opts)
+          LazyVim.mini.ai_whichkey(opts)
         end)
       end)
     end,
