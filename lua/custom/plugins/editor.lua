27286dd90c7ba8a89f86b28f4510f8312f8c12b6
wochap <gean.marroquin@gmail.com>
2025-02-04 10:00:10 -0500

    feat: integrate flash.nvim with snacks picker

--- lua/custom/plugins/editor.lua
+++ lua/custom/plugins/editor.lua
@@ -1631,6 +1631,42 @@ return {
       },
     },
   },
+  {
+    "folke/snacks.nvim",
+    optional = true,
+    opts = {
+      picker = {
+        actions = {
+          flash = function(picker)
+            require("flash").jump {
+              pattern = "^",
+              label = { after = { 0, 0 } },
+              search = {
+                mode = "search",
+                exclude = {
+                  function(win)
+                    return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "snacks_picker_list"
+                  end,
+                },
+              },
+              action = function(match)
+                local idx = picker.list:row2idx(match.pos[1])
+                picker.list:_move(idx, true, true)
+              end,
+            }
+          end,
+        },
+        win = {
+          input = {
+            keys = {
+              ["<a-s>"] = { "flash", mode = { "n", "i" } },
+              ["s"] = { "flash" },
+            },
+          },
+        },
+      },
+    },
+  },
 
   {
     "wochap/git-conflict.nvim",
