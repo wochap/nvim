1acdd7ad7988aeab412d65beced332a923dd04fb
wochap <gean.marroquin@gmail.com>
2025-03-08 18:12:55 -0500

    feat(ruby) switch to ruby_lsp

--- lua/custom/plugins/lang-ruby.lua
+++ lua/custom/plugins/lang-ruby.lua
@@ -1,11 +1,23 @@
 local constants = require "custom.utils.constants"
 
-vim.g.lazyvim_ruby_lsp = "solargraph"
+vim.g.lazyvim_ruby_lsp = "ruby_lsp"
 vim.g.lazyvim_ruby_formatter = "rubocop"
 
 return {
   {
     enabled = not constants.first_install,
     import = "lazyvim.plugins.extras.lang.ruby",
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    optional = true,
+    opts = {
+      servers = {
+        ruby_lsp = {
+          cmd_env = { BUNDLE_GEMFILE = vim.fn.getenv "GLOBAL_GEMFILE" },
+        },
+      },
+    },
   },
 }
