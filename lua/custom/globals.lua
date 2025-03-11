c06606b2c906ee98a3a2469f68b130619cbe095b
wochap <gean.marroquin@gmail.com>
2025-03-11 15:58:07 -0500

    feat: disable some default providers

--- lua/custom/globals.lua
+++ lua/custom/globals.lua
@@ -10,6 +10,12 @@ end
 -- open nvim in lazygit terminal
 vim.env.GIT_EDITOR = "nvim"
 
+-- disable some default providers
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 -- disable netrw
 if constants.disable_netrw then
   vim.g.loaded_netrw = 1
