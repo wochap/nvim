c089214840b71083806e0ca865eeb8c5c3149680
wochap <gean.marroquin@gmail.com>
2025-04-11 18:18:24 -0500

    feat: add startup profile

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,3 +1,16 @@
+-- profile startup
+if vim.env.PROF then
+  local snacks = vim.fn.stdpath "data" .. "/lazy/snacks.nvim"
+  vim.opt.rtp:append(snacks)
+  require("snacks.profiler").startup {
+    startup = {
+      -- event = "VimEnter",
+      -- event = "UIEnter",
+      event = "VeryLazy",
+    },
+  }
+end
+
 -- enable experimental Lua module loader
 vim.loader.enable()
 
