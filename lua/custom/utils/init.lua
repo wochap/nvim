23c1384f3b6ecac2c7541c976d978db36db537bf
wochap <gean.marroquin@gmail.com>
2025-04-11 15:14:44 -0500

    feat: add set_timeout

--- lua/custom/utils/init.lua
+++ lua/custom/utils/init.lua
@@ -127,4 +127,16 @@ M.get_visual_selection = function()
   return nil
 end
 
+M.set_timeout = function(ms, callback)
+  local timer = vim.loop.new_timer()
+  timer:start(ms, 0, function()
+    -- Stop and close the timer
+    timer:stop()
+    timer:close()
+
+    -- Schedule the callback to run on the main thread
+    vim.schedule(callback)
+  end)
+end
+
 return M
