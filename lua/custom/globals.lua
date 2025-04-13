699a72022ca7c9563205f2060b4be6b3a240bab2
wochap <gean.marroquin@gmail.com>
2025-04-13 12:09:34 -0500

    refactor(catppuccin) terminal colors

--- lua/custom/globals.lua
+++ lua/custom/globals.lua
@@ -1,3 +1,4 @@
+local lazyUtils = require "custom.utils.lazy"
 local constants = require "custom.utils.constants"
 
 if constants.in_nix then
@@ -25,23 +26,6 @@ if constants.disable_netrw then
   vim.g.loaded_netrw = 1
   vim.g.loaded_netrwPlugin = 1
 end
-
-vim.g.terminal_color_0 = "#45475A"
-vim.g.terminal_color_1 = "#F38BA8"
-vim.g.terminal_color_2 = "#A6E3A1"
-vim.g.terminal_color_3 = "#F9E2AF"
-vim.g.terminal_color_4 = "#89B4FA"
-vim.g.terminal_color_5 = "#F5C2E7"
-vim.g.terminal_color_6 = "#94E2D5"
-vim.g.terminal_color_7 = "#BAC2DE"
-vim.g.terminal_color_8 = "#585B70"
-vim.g.terminal_color_9 = "#F38BA8"
-vim.g.terminal_color_10 = "#A6E3A1"
-vim.g.terminal_color_11 = "#F9E2AF"
-vim.g.terminal_color_12 = "#89B4FA"
-vim.g.terminal_color_13 = "#F5C2E7"
-vim.g.terminal_color_14 = "#94E2D5"
-vim.g.terminal_color_15 = "#A6ADC8"
 
 if constants.in_neovide then
   vim.g.neovide_floating_shadow = false
--- lua/custom/plugins/colorscheme/init.lua
+++ lua/custom/plugins/colorscheme/init.lua
@@ -1,3 +1,4 @@
+local lazyUtils = require "custom.utils.lazy"
 local highlight_overrides = require "custom.plugins.colorscheme.hl_overrides"
 local constants = require "custom.utils.constants"
 
@@ -6,10 +7,38 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     event = "LazyFile",
+    init = function()
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "catppuccin-*",
+        callback = function()
+          lazyUtils.on_load("catppuccin", function()
+            local C = require("catppuccin.palettes").get_palette()
+
+            -- TODO: add custom colors for latte
+            vim.g.terminal_color_0 = C.surface1
+            vim.g.terminal_color_1 = C.red
+            vim.g.terminal_color_2 = C.green
+            vim.g.terminal_color_3 = C.yellow
+            vim.g.terminal_color_4 = C.blue
+            vim.g.terminal_color_5 = C.pink
+            vim.g.terminal_color_6 = C.teal
+            vim.g.terminal_color_7 = C.subtext1
+            vim.g.terminal_color_8 = C.surface2
+            vim.g.terminal_color_9 = C.red
+            vim.g.terminal_color_10 = C.green
+            vim.g.terminal_color_11 = C.yellow
+            vim.g.terminal_color_12 = C.blue
+            vim.g.terminal_color_13 = C.pink
+            vim.g.terminal_color_14 = C.teal
+            vim.g.terminal_color_15 = C.subtext0
+          end)
+        end,
+      })
+    end,
     opts = {
       transparent_background = constants.transparent_background,
       show_end_of_buffer = false,
-      term_colors = true,
+      term_colors = false,
       dim_inactive = {
         enabled = false,
       },
