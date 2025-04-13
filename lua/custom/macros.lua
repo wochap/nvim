a5181fc96009c75e9ece8c9526958828f40d7124
wochap <gean.marroquin@gmail.com>
2025-04-13 16:02:11 -0500

    feat: add marcro for commonjs to es module

--- lua/custom/macros.lua
+++ lua/custom/macros.lua
@@ -10,3 +10,15 @@ map("n", ",l", function()
   vim.cmd "normal! @l"
   vim.fn.setreg("l", prev_l, prev_ltype)
 end, "markdown link", { noremap = true })
+
+map("n", ",i", function()
+  local register_key = "i"
+  local prev_register = vim.fn.getreg(register_key)
+  local prev_register_type = vim.fn.getregtype(register_key)
+  -- stylua: ignore
+  vim.fn.setreg(register_key, "^cwimportf=€ý5vf(€ý5cfrom AF€ý50f)€ý5C")
+  -- stylua: ignore end
+  vim.cmd("normal! @" .. register_key)
+  vim.fn.setreg(register_key, prev_register, prev_register_type)
+end, "require to import", { noremap = true })
+
