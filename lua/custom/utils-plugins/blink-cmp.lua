28255f14f125e6e9c330d65d0021864b4f1b312d
wochap <gean.marroquin@gmail.com>
2025-04-13 21:26:52 -0500

    fix typo

--- lua/custom/utils-plugins/blink-cmp.lua
+++ lua/custom/utils-plugins/blink-cmp.lua
@@ -101,8 +101,11 @@ M.inside_comment_block = function()
   end
   local node_under_cursor = vim.treesitter.get_node()
   local parser = vim.treesitter.get_parser(nil, nil, { error = false })
+  if not parser then
+    return false
+  end
   local query = vim.treesitter.query.get(parser:lang(), "highlights")
-  if not parser or not node_under_cursor or not query then
+  if not node_under_cursor or not query then
     return false
   end
   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
