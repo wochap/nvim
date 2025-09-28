af2fda051c4816d6c37f6ad3e86115077bde5b48
wochap <gean.marroquin@gmail.com>
2025-09-28 11:48:33 -0500

    fix: empty buffer when opening notes with zk

--- lua/custom/utils/constants.lua
+++ lua/custom/utils/constants.lua
@@ -110,22 +110,18 @@ M.in_kitty = os.getenv "TERM" == "xterm-kitty"
 
 M.in_neovide = vim.g.neovide
 
-local zk_arg = "zk"
-M.in_zk = zk_arg == vim.fn.argv()[1]
+M.in_zk = os.getenv "IN_ZK" == "true"
 
-local obsidian_arg = "obsidian"
-M.in_obsidian = obsidian_arg == vim.fn.argv()[1]
+M.in_obsidian = os.getenv "IN_OBSIDIAN" == "true"
 
 local leet_arg = "leetcode.nvim"
 M.in_leetcode = leet_arg == vim.fn.argv()[1]
 
-local kitty_arg = "kitty-scrollback"
-M.in_kittyscrollback = kitty_arg == vim.fn.argv()[1]
+M.in_kittyscrollback = os.getenv "IN_KITTYSCROLLBACK" == "true"
 
 -- nvim is opening a file from the cmdline
-M.has_file_arg = vim.fn.argc(-1) > 0 and not M.in_kittyscrollback
+M.has_file_arg = vim.fn.argc(-1) > 0
 
--- M.transparent_background = not constants.in_neovide
 M.transparent_background = os.getenv "TRANSPARENT" == "true" and true or false
 
 -- TODO: makes more elements transparent
@@ -144,8 +140,10 @@ M.big_file_mb = 0.5
 local nix_path = os.getenv "NIX_PATH"
 M.in_nix = nix_path ~= nil and nix_path ~= ""
 
+-- disables some plugins for faster editing
 M.in_lite = os.getenv "LITE" == "true"
 
+-- disables more plugins for faster editing
 -- called from zvm_vi_edit_command_line
 M.in_vi_edit = os.getenv "IN_VI_EDIT" == "true"
 
