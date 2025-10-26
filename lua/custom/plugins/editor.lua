f8fb6bc4c67428c22b669b7532033662057c09a4
wochap <gean.marroquin@gmail.com>
2025-10-26 16:17:04 -0500

    feat(neo-tree.nvim): overwrite delete commands to use trash-put cmd

--- lua/custom/plugins/editor.lua
+++ lua/custom/plugins/editor.lua
@@ -207,6 +207,39 @@ return {
             "node_modules",
           },
         },
+        commands = {
+          delete = function(state)
+            local utils = require "neo-tree.utils"
+            local inputs = require "neo-tree.ui.inputs"
+            local path = state.tree:get_node().path
+            local _, name = utils.split_path(path)
+            local msg = string.format("Are you sure you want to trash '%s'?", name)
+
+            inputs.confirm(msg, function(confirmed)
+              if not confirmed then
+                return
+              end
+
+              vim.fn.system { "trash-put", vim.fn.fnameescape(path) }
+              require("neo-tree.sources.manager").refresh(state.name)
+            end)
+          end,
+          delete_visual = function(state, selected_nodes)
+            local inputs = require "neo-tree.ui.inputs"
+            local msg = "Are you sure you want to trash " .. #selected_nodes .. " files ?"
+
+            inputs.confirm(msg, function(confirmed)
+              if not confirmed then
+                return
+              end
+
+              for _, node in ipairs(selected_nodes) do
+                vim.fn.system { "trash-put", vim.fn.fnameescape(node.path) }
+              end
+              require("neo-tree.sources.manager").refresh(state.name)
+            end)
+          end,
+        },
       },
       git_status = {
         window = {
