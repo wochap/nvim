ba3bd5e44f71fe3bbac8f07e7e5c2ff04e5ee899
wochap <gean.marroquin@gmail.com>
2026-08-10 18:54:00 -0500

    fix , binding

--- lua/custom/macros.lua
+++ lua/custom/macros.lua
@@ -1,7 +1,7 @@
 local keymaps_utils = require "custom.utils.keymaps"
 local map = keymaps_utils.map
 
-map("n", ",l", function()
+map("n", "<leader>m,l", function()
   local prev_l = vim.fn.getreg "l"
   local prev_ltype = vim.fn.getregtype "l"
   -- stylua: ignore
@@ -11,7 +11,7 @@ map("n", ",l", function()
   vim.fn.setreg("l", prev_l, prev_ltype)
 end, "markdown link", { noremap = true })
 
-map("n", ",i", function()
+map("n", "<leader>m,i", function()
   local register_key = "i"
   local prev_register = vim.fn.getreg(register_key)
   local prev_register_type = vim.fn.getregtype(register_key)
@@ -21,4 +21,3 @@ map("n", ",i", function()
   vim.cmd("normal! @" .. register_key)
   vim.fn.setreg(register_key, prev_register, prev_register_type)
 end, "require to import", { noremap = true })
-
